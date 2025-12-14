67b3ca51ed1625dd25bb3e7b971e7493c0a6b1c4
akshansh7488 <akshanshrajput1@gmail.com>
2025-12-14 13:18:17 +0530

    Update maxElement.cpp

--- maxElement.cpp
+++ maxElement.cpp
@@ -1,1 +1,13 @@
+// Find maximum element in an array
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+int findMax(int arr[], int n) {
+    int maxVal = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}

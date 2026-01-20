3a62eaa3dd142d9feba3efb27bd122441fed6aa5
Akshanksh <akshanshrajput1@gmail.com>
2026-01-20 13:24:08 +0530

    Create removeDuplicates.cpp

---
+++ Day4/removeDuplicates.cpp
@@ -0,0 +1,12 @@
+int removeDuplicates(int arr[], int n) {
+    if (n == 0) return 0;
+
+    int i = 0;
+    for (int j = 1; j < n; j++) {
+        if (arr[j] != arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}

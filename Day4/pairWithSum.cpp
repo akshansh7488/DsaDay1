2e11952a13aad3c24d2d7e31ae195a92b3e40bf2
Akshanksh <akshanshrajput1@gmail.com>
2026-01-20 13:23:45 +0530

    Create pairWithSum.cpp

---
+++ Day4/pairWithSum.cpp
@@ -0,0 +1,10 @@
+bool pairWithSum(int arr[], int n, int target) {
+    int i = 0, j = n - 1;
+    while (i < j) {
+        int sum = arr[i] + arr[j];
+        if (sum == target) return true;
+        else if (sum < target) i++;
+        else j--;
+    }
+    return false;
+}
